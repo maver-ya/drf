fc5851048a7514547f7d0db701231827e82e9edd
maver-ya <maver2005@ya.ru>
2024-11-12 00:51:28 +0300

    (V14) JWTToken

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from movie.views import *
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +10,10 @@ urlpatterns = [
     path('api/v1/movie/', MovieAPIList.as_view()),
     path('api/v1/movie/<int:pk>/', MovieAPIUpdate.as_view()),
     path('api/v1/moviedelete/<int:pk>/', MovieAPIDestroy.as_view()),
-    path('api/v1/auth/', include('djoser.urls')),  # new
-    re_path(r'^auth/', include('djoser.urls.authtoken')),  # new
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
 ]
