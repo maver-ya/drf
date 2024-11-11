21d85c601b32dafb3feefeaded25ff29d74ac357
maver-ya <maver2005@ya.ru>
2024-11-12 00:55:29 +0300

    (V16) Pagination

--- drfsite/movie/views.py
+++ drfsite/movie/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, viewsets, mixins
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,18 +13,28 @@ from .models import Movie, Category
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import MovieSerializer
 
+
+class MovieAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+
 class MovieAPIList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = MovieAPIListPagination
+
 
 class MovieAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
     # authentication_classes = (TokenAuthentication, )
+
 
 class MovieAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = (IsAdminOrReadOnly, )
+    permission_classes = (IsAdminOrReadOnly,)
