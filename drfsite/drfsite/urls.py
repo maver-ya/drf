d7191a7fe667d7e471e9c8fde0f0aa9f528cfffb
maver-ya <maver2005@ya.ru>
2024-11-11 23:12:59 +0300

    (V8) ViewSet

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,28 +1,15 @@
-"""
-URL configuration for drfsite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from movie.views import MovieAPIView, MovieAPIList, MovieAPIUpdate, MovieAPIDetailView
+from movie.views import *
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'movie', MovieViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/movielist', MovieAPIList.as_view()),
-    path('api/v1/movielist/<int:pk>', MovieAPIUpdate.as_view()),
-    path('api/v1/moviedetail/<int:pk>', MovieAPIDetailView.as_view()),
-
+    path('api/v1/', include(router.urls))
+    # path('api/v1/movielist', MovieAPIViewSet.as_view({'get': 'list'})),
+    # path('api/v1/movielist/<int:pk>', MovieAPIViewSet.as_view({'put': 'update'})),
 ]
--- drfsite/movie/views.py
+++ drfsite/movie/views.py
@@ -2,7 +2,7 @@ from django.core.serializers import serialize
 from django.forms import model_to_dict
 from django.shortcuts import render
 from django.template.defaultfilters import title
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,54 +12,25 @@ from .models import Movie
 from .serializers import MovieSerializer
 
 
-# class MovieAPIView(generics.ListAPIView):
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+# class MovieAPIList(generics, ListCreateAPIView):
+#     queryset = Movie.objects.all()
+#     serializer_class = MovieSerializer
+#
+#
+# class MovieAPIUpdate(generics.UpdateAPIView):
+#     queryset = Movie.objects.all()
+#     serializer_class = MovieSerializer
+#
+#
+# class MovieAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Movie.objects.all()
 #     serializer_class = MovieSerializer
 
-
-class MovieAPIList(generics, ListCreateAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
-
-class MovieAPIUpdate(generics.UpdateAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
-
-class MovieAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
-
-# class MovieAPIView(APIView):
-#     def get(self, request):
-#         m = Movie.objects.all()
-#         return Response({'post': MovieSerializer(m, many=True).data})
-#
-#     def post(self, request):
-#         serializer = MovieSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#
-#         return Response({'post': serializer.data})
-#
-#     def put(self, request, *args, **kwargs):
-#         pk = kwargs.get("pk", None)
-#         if not pk:
-#             return Response({'error': "Methon PUT not allowed"})
-#         try:
-#             instance = Movie.objects.get(pk=pk)
-#         except:
-#             return Response({'error': "Methon PUT not allowed"})
-#
-#         serializer = MovieSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({"post": serializer.data})
-#
-#     def delete(self, request, *args, **kwargs):
-#         pk = kwargs.get("pk", None)
-#         if not pk:
-#             return Response({'error': "Method DELETE not allowed"})
-#
-#         Movie.objects.filter(pk=pk).delete()
-#
-#         return Response({"post": "delete post" + str(pk)})
+# class MovieAPIView(generics.ListAPIView):
+#     queryset = Movie.objects.all()
+#     serializer_class = MovieSerializer
